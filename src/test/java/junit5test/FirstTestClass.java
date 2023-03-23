1abd51c15f803a95228640f42756790c4461bba7
bismasatrian@gmail.com <bisma@mifx.com>
2023-03-23 23:43:56 +0700

    init commit

---
+++ src/test/java/junit5test/FirstTestClass.java
@@ -0,0 +1,17 @@
+package junit5test;
+
+import org.junit.jupiter.api.Test;
+
+public class FirstTestClass {
+
+    @Test
+    public void firstMethod(){
+        System.out.println("First method");
+
+    }
+
+    @Test
+    public void secondMethod(){
+        System.out.println("Second method");
+    }
+}
